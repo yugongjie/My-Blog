8fa7691c79e7f7f6df38fa940b5607d25fd8aa41
yugongjie <yugongjie@gmail.com>
2016-09-12 01:30:50 +0800

    修正返回顶部按钮bug

--- js/main.js
+++ js/main.js
@@ -13,32 +13,35 @@ $(document).ready(function(){
 	$('.contactme').click(function(){
         pagego('contactme');
 	});	
-
-	$('#bktop').click(function(){
-        pagego('banner');
-	});	
-
     function pagego(page){
 	    $('html,body').animate({
 	    	scrollTop:$('#'+page).offset().top
 	    },1000);        
-    }
+    };
+
+	$('#bktop').click(function(){
+		$('html,body').animate({
+			scrollTop:0
+		},1000);
+	});
 
     /*监听滚动条并显示或隐藏返回顶部按钮*/
 	$(window).scroll(function(){
 		var top = $(document).scrollTop();
-        if (top>=0.6*pagetop){
-        	$('#bktop').css('display','block')
+		console.log(top)
+        if (top>0.6*pagetop){
+        	$('#bktop').fadeIn(300)
         }else{
-        	$('#bktop').css('display','none')
-        }
-	})
-
+        	$('#bktop').fadeOut(300)
+        };
+	});
+    
+    // 右上角微信及推特的提示框弹出及隐藏
 	$('.wechat').click(function(){
-        $('.my-wechat').toggle(300)
+        $('.my-wechat').slideToggle(300)
 	});
 	$('.wechat').blur(function(){
-		$('.my-wechat').hide(300)
+		$('.my-wechat').slideUp(300)
 	})
 	$('.twitter').click(function(){
         $('.not-found').slideToggle(300)
