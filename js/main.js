8f3c84e803a018778a942593a489d914311335d3
yugongjie <yugongjie@gmail.com>
2016-09-11 21:14:56 +0800

    增加了右上角微信及推特弹出提示，增加了若干张背景图备选

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var pagetop=$(window).height();
+	
 	/*页面跳转模块*/
 	$('.aboutthis').click(function(){
         pagego('aboutthis');
@@ -26,10 +27,24 @@ $(document).ready(function(){
     /*监听滚动条并显示或隐藏返回顶部按钮*/
 	$(window).scroll(function(){
 		var top = $(document).scrollTop();
-        if (top>=0.8*pagetop){
+        if (top>=0.6*pagetop){
         	$('#bktop').css('display','block')
         }else{
         	$('#bktop').css('display','none')
         }
-	})	
+	})
+
+	$('.wechat').click(function(){
+        $('.my-wechat').toggle(300)
+	});
+	$('.wechat').blur(function(){
+		$('.my-wechat').hide(300)
+	})
+	$('.twitter').click(function(){
+        $('.not-found').slideToggle(300)
+	});
+	$('.twitter').blur(function(){
+		$('.not-found').slideUp(300)
+	})
+
 });
